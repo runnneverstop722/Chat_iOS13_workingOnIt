5fcc0a214d0d3ba80c233f1b93e50170c4eef3c7
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-02-18 22:30:50 +0900

    Fetching time-sorted messages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -37,7 +37,9 @@ class ChatViewController: UIViewController {
     func loadMessages() {
         
         //Load messages
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
             
             self.messages = [] // clear messages
             
@@ -68,7 +70,8 @@ class ChatViewController: UIViewController {
             
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]) { error in
                 if let e = error {
                     print("There is a issue saving data to firestore, \(e)")
