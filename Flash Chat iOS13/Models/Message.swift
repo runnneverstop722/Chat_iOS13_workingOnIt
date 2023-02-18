1831e96446039747ef0247d717135ed8515917dd
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-02-18 20:02:41 +0900

    Init to fetching new messages from the FireStore
    The cocoapod files are excluded.

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Taehoon Lee on 2023/02/17.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // 1@2.com
+    let body: String
+}
